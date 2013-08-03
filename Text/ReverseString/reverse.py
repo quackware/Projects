676baf574b2617dfdfa984ab371bf4b8036b8e9a
quackware <quackware@gmail.com>
2013-08-03 15:24:48 -0400

    Updated to use input rather than raw_input

--- Text/ReverseString/reverse.py
+++ Text/ReverseString/reverse.py
@@ -1,10 +1,10 @@
 
 def main():
-    input = raw_input("Enter a string to reverse or Exit.\n")
+    inputString = input("Enter a string to reverse or Exit.\n")
     while input != "Exit":
-        reverse = input[::-1]
+        reverse = inputString[::-1]
         print("Reverse: " + reverse)
-        input = raw_input("Enter a string to reverse or Exit.\n")
+        inputString = input("Enter a string to reverse or Exit.\n")
 
 if __name__ == "__main__":
     main()
